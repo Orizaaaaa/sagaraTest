f4a1c96d73997d3e8336c3282abb34a59feb9108
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-08-20 22:59:04 +0700

    finish all

--- src/components/fragments/home/AboutUs.tsx
+++ src/components/fragments/home/AboutUs.tsx
@@ -3,7 +3,10 @@ const AboutUs = () => {
     return (
         <div className="container mx-auto py-20">
             <div className="grid grid-cols-1 lg:grid-cols-6 gap-2 lg:gap-27">
-                <div className="lg:col-span-1 flex justify-center items-center border-t-2 border-black">
+                <div className="lg:col-span-1 flex flex-col justify-center items-center ">
+                    <div className="flex justify-center">
+                        <hr className="border-3 rounded-md border-black my-5 w-22" />
+                    </div>
                     <h1 className=" text-xl lg:text-2xl  font-medium" >About Us</h1>
                 </div>
                 <div className="lg:col-span-5 px-2 lg:px-0 ">
